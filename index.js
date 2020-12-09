8484a7f2b4626da96ab378297d04b5a22e491e2b
duongtai2404 <maiduongtai.itnlu.com>
2020-12-09 17:33:34 +0700

    fix

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+//fix
 //config connect db
 let mysql = require('mysql');
 let mysqlConnection = mysql.createConnection({
